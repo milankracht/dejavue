5b168956ed859f0b0efefc28d9eb58d1fb9fb76c
Milan Kracht <milan@domini.nl>
2020-06-20 13:14:29 +0200

    Shaka player installed

--- components/home/TrailerModal.vue
+++ components/home/TrailerModal.vue
@@ -2,15 +2,58 @@
   <div class="trailer-modal" :class="{ visible: showTrailerModal }">
     <div class="container">
       <h3>Movie title</h3>
+      <video id="video" width="640" poster="//shaka-player-demo.appspot.com/assets/poster.jpg" controls autoplay></video>
       <p @click="$emit('handleHideTrailerModal')">Close</p>
     </div>
   </div>
 </template>
 
 <script>
+import shaka from 'shaka-player/dist/shaka-player.compiled.js'
+
 export default {
   props: {
     showTrailerModal: Boolean
+  },
+  watch: {
+    showTrailerModal (value) {
+      if (value) {
+        this.initShaka()
+      } else {
+        this.destroyPlayer()
+      }
+    }
+  },
+  data () {
+    return {
+      manifestUri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    }
+  },
+  methods: {
+    initShaka () {
+      shaka.polyfill.installAll()
+
+      if (shaka.Player.isBrowserSupported()) {
+        this.initPlayer()
+      } else {
+        console.error('Browser not supported!')
+      }
+    },
+    initPlayer () {
+      const video = document.getElementById('video')
+      const player = new shaka.Player(video)
+
+      window.player = player
+
+      player.load(this.manifestUri)
+        .then(function () {
+          console.log('The video has now been loaded!')
+        })
+        .catch()
+    },
+    destroyPlayer () {
+      window.player.destroy()
+    }
   }
 }
 </script>
