6fb28d354b175adbcb42f2a484a1e72d17a4cccf
Milan Kracht <milan@domini.nl>
2020-06-22 12:28:23 +0200

    Final unit test on search results

---
+++ test/home/SearchResults.spec.js
@@ -0,0 +1,69 @@
+import { shallowMount } from '@vue/test-utils'
+import SearchResults from '@/components/home/SearchResults.vue'
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('SearchResults', () => {
+  let wrapper = shallowMount(SearchResults, {
+    propsData: {
+      showSearchResults: true,
+      search: 'foo'
+    }
+  })
+
+  test('is a Vue instance', () => {
+    expect(wrapper.text()).toMatch('Search results')
+  })
+
+  test('initial settings when search is set', () => {
+    expect(wrapper.vm.page).toEqual(1)
+    expect(wrapper.vm.list.length).toEqual(0)
+    expect(wrapper.vm.loadMore).toEqual(false)
+  })
+
+  describe ('searchForPrograms', () => {
+    describe('succesful api call', () => {
+      wrapper = null
+
+      const response = { 
+        data: [
+          {
+            id: 1,
+            title: 'foo'
+          },
+          {
+            id: 2,
+            title: 'bar'
+          }
+        ]
+      }
+  
+      axios.get.mockResolvedValue(response)
+  
+      wrapper = shallowMount(SearchResults, {
+        propsData: {
+          showSearchResults: true,
+          search: 'foo'
+        }
+      })
+    })
+  
+    afterEach(() => {
+      jest.resetModules()
+      jest.clearAllMocks()
+    })
+
+    test('successful searchForPrograms api call', async() => {
+      wrapper.vm.searchForPrograms()
+      
+      expect(axios.get).toBeCalledWith(expect.stringMatching(/foo/))
+      expect(axios.get).toHaveBeenCalledTimes(1)
+
+      wrapper.vm.$nextTick().then(function () {
+        expect(wrapper.vm.list).toEqual(response.data)
+        expect(wrapper.vm.loadMore).toEqual(false)
+      })
+    })
+  })
+})
